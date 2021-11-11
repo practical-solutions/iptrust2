59aaf5c88db73bbf83ee0b1047fb4dab637089bd
Gero Gothe <practical@medizin-lernen.de>
2021-11-11 13:15:48 +0100

    Added publicaccess option

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # IP-Trust-Manager II
 
-This is a fork of the original Plugin by Andriy Lesyuk. It has been modified by Gero Gothe <gero.gothe [at] medizin-lernen.de>, as the original ist not working since
+This is a fork of the original Plugin by Andriy Lesyuk. It has been modified by Gero Gothe <gero.gothe [at] medizindoku.de>, as the original ist not working since
 DokuWiki/**Hogfather**.
 
 Thus, this software ist also under the GPL-License.
+
+## Additional functionality
+
+You can create a group ```@publicaccess```. If a namespace has ```read```-rights for this group, it can be **publicly accessed by everybody**, whereas the ```@ALL```-group is only meant for logged in users or those accessing the wiki from the defined IPs.
+
+Public access is only granted for those pages, who have ```read```-privileges in the ```@ALL``` group as well, which means: for public access there have to be ```read```-rights in both the ```@ALL``` and the ```@publicaccess``` group.
--- action.php
+++ action.php
@@ -20,22 +20,42 @@ class action_plugin_iptrust2 extends DokuWiki_Action_Plugin {
      */
     function register(Doku_Event_Handler $controller) {
         $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_act_preprocess', array());
+        $controller->register_hook('AUTH_ACL_CHECK', 'AFTER', $this, 'ip_group', array());
+        
     }
+
+
+    function ip_group(&$event, $param) {
+        $ip = clientIP(true);
+    }
+
 
     /**
      * Check if content should be shown
      */
     function handle_act_preprocess(&$event, $param) {
         global $conf;
+      
+
         if (!isset($_SERVER['REMOTE_USER'])) {
+
             if (!in_array($event->data, array('login', 'register', 'resendpwd'))) {
                 $ip = clientIP(true);
                 $ips = @file(DOKU_CONF.'iptrust.conf', FILE_SKIP_EMPTY_LINES);
                 if (!$ips || !in_array($ip."\n", $ips)) {
-                    $event->data = 'login';
+                    
+                    # Allow access, if allowed for the group @publicaccess
+                    global $ID;
+                    $perms = $this->aclcheck($ID);
+                    print_r($perms);
+                    
+                    if (!$perms['@publicaccess'] == 1) $event->data = 'login';
+                    
                 }
+
             }
         } else {
+
             if ($event->data == 'login') {
                 $nets = $this->getConf('log_networks');
                 if ($nets) {
@@ -66,6 +86,64 @@ class action_plugin_iptrust2 extends DokuWiki_Action_Plugin {
                 }
             }
         }
+    }
+
+
+    /** This function is from the aclinfo-plug by Andreas Gohr
+     * Version 2020-10-01
+     * https://www.dokuwiki.org/plugin:aclinfo
+     */
+    function aclcheck($id){
+        global $conf;
+        global $AUTH_ACL;
+
+        $id    = cleanID($id);
+        $ns    = getNS($id);
+        $perms = array();
+
+        //check exact match first
+        $matches = preg_grep('/^'.preg_quote($id,'/').'\s+/',$AUTH_ACL);
+        if(count($matches)){
+            foreach($matches as $match){
+                $match = preg_replace('/#.*$/','',$match); //ignore comments
+                $acl   = preg_split('/\s+/',$match);
+                if($acl[2] > AUTH_DELETE) $acl[2] = AUTH_DELETE; //no admins in the ACL!
+                if(!isset($perms[$acl[1]])) $perms[$acl[1]] = $acl[2];
+            }
+        }
+
+        //still here? do the namespace checks
+        if($ns){
+            $path = $ns.':\*';
+        }else{
+            $path = '\*'; //root document
+        }
+
+        do{
+            $matches = preg_grep('/^'.$path.'\s+/',$AUTH_ACL);
+            if(count($matches)){
+                foreach($matches as $match){
+                    $match = preg_replace('/#.*$/','',$match); //ignore comments
+                    $acl   = preg_split('/\s+/',$match);
+                    if($acl[2] > AUTH_DELETE) $acl[2] = AUTH_DELETE; //no admins in the ACL!
+                    if(!isset($perms[$acl[1]])) $perms[$acl[1]] = $acl[2];
+                }
+            }
+
+            //get next higher namespace
+            $ns   = getNS($ns);
+
+            if($path != '\*'){
+                $path = $ns.':\*';
+                if($path == ':\*') $path = '\*';
+            }else{
+                //we did this already
+                //break here
+                break;
+            }
+        }while(1); //this should never loop endless
+
+        return $perms;
     }
 
 }
